b3a1aaa325779f52baef5e3e3d6e1bae317a90b0
Aman Pandya <amanpandya712@gmail.com>
2023-11-16 19:24:07 +0000

    feat: added new table user_token to the codebase for login management

--- server/app/db/sql/models.py
+++ server/app/db/sql/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 
 StatusEnum = Enum('to_do', 'in_progress', 'done', name='task_status')
 PriorityEnum = Enum('critical','high','medium','low', name='task_priority')
+TokenTypeEnum = Enum('authentication','verification','forgot_password', name='token_type')
 
 class BaseModel(base.Base):
     __abstract__ = True
@@ -15,16 +16,31 @@ class User(BaseModel):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    full_name = Column(String(60))
+    full_name = Column(String(60), nullable=False)
     email = Column(String(60), nullable=False, unique=True)
     password = Column(String(30), nullable=False)
     is_active = Column(Boolean, default=True)
 
     task = relationship('Task', back_populates='user')
+    user_token = relationship('UserToken', back_populates='user')
 
     def __repr__(self):
-        return f"<User {self.full_name} ({self.email})>"
+        return f"<User {self.id})>"
 
+class UserToken(BaseModel):
+    __tablename__ = "user_token"
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String(100), nullable=False, unique=True)
+    expiry_date = Column(DateTime, nullable=False)
+    is_valid = Column(Boolean, default=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    token_type = Column(TokenTypeEnum, nullable=False)
+
+    user = relationship('User', back_populates='user_token')
+
+    def __repr__(self):
+        return f"<UserToken {self.id}>"
 
 class Task(BaseModel):
     __tablename__ = "task"
