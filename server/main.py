0735c645229ed35872344b1a4f17a8bfc46f3d25
Aman Pandya <amanpandya712@gmail.com>
2023-11-18 12:34:47 +0000

    Created Initial User Registration API & did app re-configuration for that
    - added logging module to the app
    - added functionality to check for database connection, else fail the service
    - added some functions for user authentication & related env variables
    - simplified database session management using dependency injection instead to have new connections for each API call
    - implemented User Registration end to end Flow, successfully tested that (More Validations to be added in future PR)

--- server/main.py
+++ server/main.py
@@ -1,26 +1,55 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
+from sqlalchemy import text
 
 from app.config.settings import app_settings
+from app.db.sql.config import SessionLocal
 from app.api.v1 import v1_router
+from app.misc.logger import logger
 
 allowed_origins = app_settings.allowed_origins.split(",")
 
-app = FastAPI()
-app.add_middleware(HTTPSRedirectMiddleware)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+app = FastAPI(
+    title=app_settings.app_name
 )
+app.add_middleware(HTTPSRedirectMiddleware)
+if allowed_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=allowed_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app.include_router(v1_router, prefix="/v1", tags=["v1"])
+app.include_router(v1_router, prefix="/api/v1", tags=["v1"])
 
 @app.get("/info")
 async def info():
     return {
         "app_name": app_settings.app_name
     }
+
+@retry(
+    stop=stop_after_attempt(app_settings.app_init_waiting_time),
+    wait=wait_fixed(app_settings.app_init_wait_intervals),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def init() -> None:
+    try:
+        db = SessionLocal()
+        db.execute(text("SELECT 1"))
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+def main() -> None:
+    logger.info("Initializing Service")
+    init()
+    logger.info("Service finished initializing")
+
+main()
