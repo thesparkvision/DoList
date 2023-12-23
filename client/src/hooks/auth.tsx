13d9a7ff0af3fd1acdfd891ccf49837ea42044f2
Aman <40954160+thesparkvision@users.noreply.github.com>
2023-12-23 19:08:30 +0000

    fix: fixed linting issues & also fixed authentication mechanism to ensure login checks on each render of page

--- client/src/hooks/auth.tsx
+++ client/src/hooks/auth.tsx
@@ -1,12 +1,12 @@
-function useAuth(){
-    let APP_TOKEN: string | null = localStorage.getItem("API_TOKEN")
-    let isUserAuthenticated: boolean = false
+function checkIsUserAuthenticated(): boolean {
+    const APP_TOKEN: string | null = localStorage.getItem("API_TOKEN")
+    let isUserAuthenticated = false;
 
-    if(!APP_TOKEN){
-        isUserAuthenticated = true
+    if(APP_TOKEN){
+        isUserAuthenticated = true;
     }
 
     return isUserAuthenticated
 }
 
-export default useAuth;
+export default checkIsUserAuthenticated;
