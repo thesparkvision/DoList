68c978739ddc1bdeef884e1c965307c7754abe11
Aman <40954160+thesparkvision@users.noreply.github.com>
2023-12-24 22:51:40 +0000

    feat: integrated login page with backend

--- client/src/containers/LoginPage.tsx
+++ client/src/containers/LoginPage.tsx
@@ -1,10 +1,130 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Button, Card, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { failedNotification, successNotification } from "../lib/Utils";
+import { BACKEND_URLS } from "../lib/Constants";
 import "./LoginPage.scss"
 
-function LoginPage(){
-    return  (
+interface FormData {
+    password: string;
+    email: string;
+}
+
+const initialFormData: FormData = {
+    password: '',
+    email: '',
+};
+
+function LoginPage() {
+    const navigate = useNavigate()
+
+    const [formData, setFormData] = useState<FormData>(initialFormData)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event?.preventDefault()
+
+        const requestPayload = {
+            "email": formData.email,
+            "password": formData.password
+        }
+
+        await fetch(
+            BACKEND_URLS.LOGIN_URL,
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(requestPayload)
+            }
+        ).then((response) => {
+            if (!response.ok) {
+                return response.json().then(errorData => {
+                    throw { status: response.status, errorData };
+                });
+            }
+
+            return response.json()
+        }).then(data => {
+            if(data.access_token){
+                localStorage.setItem("ACCESS_TOKEN", data.access_token)
+            }
+
+            successNotification({
+                title: "Account Authenticated Successfully! Redirecting to home page...",
+                duration: 8000
+            })
+
+            setTimeout(() => {
+                navigate('/')
+            }, 3000);
+        }).catch((error) => {
+            failedNotification({
+                title: "Something went wrong with user authentication!",
+                description: `HTTP Status: ${error.status}`
+            })
+        })
+    }
+
+    return (
         <Box id="loginPage">
-            <Text fontSize="xl" fontWeight="bold">Login Page</Text>
+            <Text fontSize="xl" fontWeight="bold">
+                Login
+            </Text>
+            <br />
+
+            <Card id="loginBox">
+                <form onSubmit={handleSubmit}>
+                    <FormControl
+                        className="formControl"
+                        isRequired
+                    >
+                        <FormLabel mb='8px'>Email Address: </FormLabel>
+                        <Input
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            placeholder='Enter email address here'
+                            size='sm'
+                            maxLength={30}
+                        />
+                    </FormControl>
+                    <br />
+
+                    <FormControl
+                        className="formControl"
+                        isRequired
+                    >
+                        <FormLabel mb='8px'>Password: </FormLabel>
+                        <Input
+                            name="password"
+                            value={formData.password}
+                            type={'text'}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            placeholder='Enter your password here'
+                            size='sm'
+                            minLength={6}
+                            maxLength={25}
+                        />
+                    </FormControl>
+                    <br />
+
+                    <Button
+                        id="submitBtn"
+                        type="submit"
+                        colorScheme='teal'
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </Card>
         </Box>
     )
 }
