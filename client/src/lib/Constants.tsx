37eadb8e337045081bbaec11a4c6fbad32b380b0
Aman <40954160+thesparkvision@users.noreply.github.com>
2023-12-25 23:34:31 +0000

    feat: modeled the Task Page components layout in frontend & wrapped up most of the styling

--- client/src/lib/Constants.tsx
+++ client/src/lib/Constants.tsx
@@ -1,7 +1,53 @@
 import { API_URL } from "./Config";
 
-export const BACKEND_URLS = {
+export const BACKEND_URLS: Record<string, string> = {
     SIGNUP_URL: API_URL + "/api/v1/users/register",
     LOGIN_URL: API_URL + "/api/v1/users/login",
     GET_USER_URL: API_URL + "/api/v1/users/me",
+    TASK_PAGE_URL: API_URL + "/api/v1/tasks"
+}
+
+export const FRONTEND_PAGE_URLS: Record<string, string> = {
+    SIGNUP_PAGE: "/auth/signup",
+    LOGIN_PAGE: "/auth/login",
+    HOME_PAGE: "/"
+}
+
+type SelectConstants = {
+    color: string;
+    value: string;
+};
+
+export const PRIORITY_VALUES : Record<string, SelectConstants> = {
+    "CRITICAL": {
+        color: "red",
+        value: "CRITICAL"
+    },
+    "HIGH": {
+        color: "orange",
+        value: "HIGH"
+    },
+    "MEDIUM": {
+        color: "yellow",
+        value: "MEDIUM"
+    },
+    "LOW": {
+        color: "blue",
+        value: "LOW"
+    }
+}
+
+export const STATUS_VALUES : Record<string, SelectConstants> = {
+    "TO_DO": {
+        color: "skyblue",
+        value: "TO_DO"
+    },
+    "IN_PROGRESS": {
+        color: "yellow",
+        value: "IN_PROGRESS"
+    },
+    "DONE": {
+        color: "green",
+        value: "DONE"
+    }
 }
